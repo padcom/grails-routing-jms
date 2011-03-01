fd0a09350a538ec1e8033961f29d63b9d0b8fa4f
Matthias Hryniszak <padcom@gmail.com>
2011-03-02 00:49:37 +0100

    Initial import

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,40 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+
+        // uncomment the below to enable remote dependency resolution
+        // from public Maven repositories
+        //mavenLocal()
+        //mavenCentral()
+        //mavenRepo "http://snapshots.repository.codehaus.org"
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+
+        // runtime 'mysql:mysql-connector-java:5.1.5'
+        runtime('org.apache.activemq:activemq-all:5.2.0') {
+            excludes 'spring-beans', 'spring-core'
+        }
+        runtime('org.apache.activemq:activemq-pool:5.2.0') {
+            excludes 'spring-beans', 'spring-core'
+        }
+        runtime('org.apache.camel:camel-jms:2.5.0') {
+            excludes 'spring-aop', 'spring-beans', 'spring-core', 'spring-expression', 'spring-asm', 'spring-tx', 'spring-context'
+        }
+    }
+}
